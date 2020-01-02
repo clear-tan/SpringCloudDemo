84c608fafaa86259ab2d79d8c81cd5b059357ac7
win 10 <win 10@Win10>
2020-01-02 23:49:18 +0800

    新增微服务负载均衡规则----

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,15 +1,27 @@
 package com.atguigu.springcloud.cfgbeans;
 
+
+import org.bouncycastle.crypto.signers.RandomDSAKCalculator;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
 @Configuration
 public class ConfigBean {
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
 	
+	@Bean
+	public IRule getRules() {
+		//Ribbon的负载均衡策略 想要哪种策略就new 哪个
+		return new RandomRule();
+	}
 	
 }
